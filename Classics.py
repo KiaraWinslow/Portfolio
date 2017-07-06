d8e16ce2a8042ffa5e4e3831aa088a39eb46a694
Kiara Winslow <Kiarawinslow@gmail.com>
2017-07-06 11:54:37 -0700

    Add files via upload

---
+++ Classics.py
@@ -0,0 +1,5 @@
+Classics = ("Wu Tang", "NWA", "2Pac", "Biggie", "DMX", "Snoop", "Dr. Dre",
+"Run-D.M.C.",   "Nas", "Public Enemy")
+
+print (Classics)
+print ("The odd artists are" + " "+ str(Classics[1::2]))
