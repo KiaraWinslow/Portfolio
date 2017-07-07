63a903e5d7b037b23e4c088dbf564cf9a6159098
Kiara Winslow <Kiarawinslow@gmail.com>
2017-07-07 12:00:57 -0700

    Delete Classics.py

--- Classics.py
+++
@@ -1,5 +0,0 @@
-Classics = ("Wu Tang", "NWA", "2Pac", "Biggie", "DMX", "Snoop", "Dr. Dre",
-"Run-D.M.C.",   "Nas", "Public Enemy")
-
-print (Classics)
-print ("The odd artists are" + " "+ str(Classics[1::2]))
